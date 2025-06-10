77d1b0b711850cf52dd057cc5b0cc98eba93616f
BasmaElsisy <basmaelsisy@BasmaElsisys-MacBook-Pro.local>
2025-06-10 17:52:17 +0300

    updates

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import { ReactNode } from "react";
 import "../globals.css";
 import { tajwal } from "../fonts";
+import MainHeader from "../_components/Header/MainHeader";
 
 
 type Props = {
@@ -57,25 +58,25 @@ export default async function RootLayout({ children, params }: Props) {
   return (
     <html lang={locale} dir={direction}>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Tuffy:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
-              <meta charSet="utf-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1" />
-              <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
-              <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-              <link rel="shortcut icon" href="/favicon/favicon.ico" />
-              <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-              <link rel="manifest" href="/favicon/site.webmanifest" />
-            </head>
-            <body
-              className={`${locale == "ar"? tajwal.className:""}`}
-            >
-              <NextIntlClientProvider locale={locale} messages={messages}>
-                  {/* <MainHeader /> */}
-                  <main>{children}</main>
-              </NextIntlClientProvider>
-            </body>
-          </html>
-          );
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Tuffy:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
+      <body
+        className={`${locale == "ar" ? tajwal.className : ""}`}
+      >
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <MainHeader />
+          <main>{children}</main>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
--- app/_components/Header/MainHeader.tsx
+++ app/_components/Header/MainHeader.tsx
@@ -1,8 +1,63 @@
+'use client'
+import { useLocale, useTranslations } from "next-intl";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
+    const locale = useLocale()
+    const t = useTranslations();
+    const Pathname = usePathname();
+    console.log(Pathname);
+    const Menu = [
+        {
+            name: t("Menu.home"),
+            link: `${locale == "en" ? "/en" : ""}/`
+        },
+        {
+            name: t("Menu.our_story"),
+            link: `${locale == "en" ? "/en" : ""}/our-story`
+        },
+        {
+            name: t("Menu.our_com"),
+            link: `${locale == "en" ? "/en" : ""}/our-communities`
+        },
+        {
+            name: t("Menu.vision"),
+            link: `${locale == "en" ? "/en" : ""}/vision-2030`
+        },
+        {
+            name: t("Menu.lifeAtMV"),
+            link: `${locale == "en" ? "/en" : ""}/life-at-mv`
+        },
+    ]
     return (
-        <div className=' bg-white py-4 shadow-lg'>
-           
+        <div className=' fixed w-full z-20 py-2.5 border-b-[2px] border-white border-opacity-20'>
+            <div className="max-w-[1448px] px-4 mx-auto flex justify-between items-center">
+                <Link className=" relative w-[268px] aspect-[401/105]" href={`${locale == "en" ? "/en" : ""}/`}>
+                    <Image
+                        src={'/logowhite.webp'}
+                        alt="Logo MV KSA"
+                        fill
+                    />
+                </Link>
+                <div className="w-[calc(100%-268px-200px)] ">
+                    <div className="flex gap-[28px]">
+                        {Menu.map((item: { name: string, link: string }, index: number) => (
+                            <Link
+                                className={`${Pathname === item.link ? " before:opacity-100 pointer-events-none" : " before:opacity-0"} relative text-base font-bold text-white hover:text-opacity-60 duration-500 transition-all before:content-normal before:w-full before:h-[2px] before:bg-white before:absolute before:inset-x-0 before:bottom-[-35px]`}
+                                key={index}
+                                href={`${item.link}`}>
+                                {item.name}
+                            </Link>
+                        ))}
+
+                    </div>
+                </div>
+
+
+            </div>
+
         </div>
     )
 }
