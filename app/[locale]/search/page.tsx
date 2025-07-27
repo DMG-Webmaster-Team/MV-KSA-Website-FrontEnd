4cfc514879241540a685b3780c07b4c3cf59a295
BasmaElsisy <basmaelsisy@BasmaElsisys-MacBook-Pro.local>
2025-07-27 17:27:43 +0300

    updates

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -1,5 +1,6 @@
 import SearchResultsPage from "@/app/_components/MainPages/SearchPage";
 import React from "react";
+export const runtime = 'edge';
 
 export default function page() {
   return <SearchResultsPage />;
