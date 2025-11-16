7959091c2079c523f484b6a7c8cc6d9ef94ced28
VerenaMoriesM <verena.mories@mitchdesigns.com>
2025-11-16 17:08:08 +0200

    privacy page added

--- app/_components/Header/MainHeader.tsx
+++ app/_components/Header/MainHeader.tsx
@@ -86,8 +86,13 @@ export default function MainHeader({ data }: { data: Menu[] }) {
     Pathname.startsWith("/en/careers/") ||
     Pathname == "/media-center" ||
     Pathname == "/en/media-center" ||
+
     Pathname.startsWith("/media-center/") ||
     Pathname.startsWith("/en/media-center/") ||
+     Pathname == "/privacy" ||
+    Pathname == "/en/privacy" ||
+     Pathname.startsWith("/privacy/") ||
+    Pathname.startsWith("/en/privacy/") ||
     Pathname == "/search" ||
     Pathname == "/en/search";
   const isUnitPage = /^\/(?:[a-z]{2}\/)?projects\/[^/]+\/units\/[^/]+$/.test(
